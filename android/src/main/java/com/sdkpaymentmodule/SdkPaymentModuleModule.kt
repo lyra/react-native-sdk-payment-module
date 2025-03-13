cb887cd22d607ff83aa8206ebbf4e228554b7676
Thibaut-Mejane <thibaut.mejane@gmail.com>
2025-03-13 14:57:21 +0100

    feat: add process

--- android/src/main/java/com/sdkpaymentmodule/SdkPaymentModuleModule.kt
+++ android/src/main/java/com/sdkpaymentmodule/SdkPaymentModuleModule.kt
@@ -1,6 +1,7 @@
 package com.sdkpaymentmodule
 
 import android.util.Log
+import androidx.fragment.app.FragmentActivity
 import com.facebook.react.bridge.Callback
 import com.facebook.react.bridge.ReactApplicationContext
 import com.facebook.react.bridge.ReadableMap
@@ -8,6 +9,9 @@ import com.facebook.react.bridge.WritableMap
 import com.facebook.react.bridge.WritableNativeMap
 import com.facebook.react.module.annotations.ReactModule
 import com.lyra.sdk.Lyra
+import com.lyra.sdk.callback.LyraHandler
+import com.lyra.sdk.callback.LyraResponse
+import com.lyra.sdk.exception.LyraException
 import com.lyra.sdk.exception.LyraMobException
 import org.json.JSONException
 
@@ -20,7 +24,7 @@ class SdkPaymentModuleModule(reactContext: ReactApplicationContext) :
   private var context: ReactApplicationContext = reactContext
 
   override fun getFormTokenVersion(): Double {
-    Log.d("========>", "getFormTokenVersion (from native)")
+    Log.d(name, "getFormTokenVersion")
     return lyraSDK!!.getFormTokenVersion().toDouble()
   }
 
@@ -46,7 +50,53 @@ class SdkPaymentModuleModule(reactContext: ReactApplicationContext) :
   }
 
   override fun process(formToken: String?, options: ReadableMap?, onSuccess: Callback?, onError: Callback?) {
-    Log.d("========>", "process (from native) $formToken")
+    Log.d(name, "process")
+    try{
+      lyraSDK!!.process((context.currentActivity as FragmentActivity).supportFragmentManager,
+        formToken!!, object : LyraHandler {
+          override fun onSuccess(lyraResponse: LyraResponse) {
+            var map: WritableMap? = null
+            try {
+              map = Util.convertJsonToMap(lyraResponse)
+            } catch (ex: JSONException) {
+              Log.e(name, ex.message, ex)
+            }
+            onSuccess!!.invoke(map)
+          }
+
+          override fun onError(lyraException: LyraException, lyraResponse: LyraResponse?) {
+            var map: WritableMap? = null
+            try {
+              map = Util.convertJsonToMap(lyraResponse)
+              val error: WritableMap = WritableNativeMap()
+              error.putString("detailErrorCode", lyraException.detailErrorCode)
+              error.putString("detailErrorMessage", lyraException.detailErrorMessage)
+              error.putString("errorCode", lyraException.errorCode)
+              error.putString("errorMessage", lyraException.errorMessage)
+              lyraException.technicalError?.let { error.putBoolean("technicalError", it) }
+              map?.putMap("error", error)
+            } catch (ex: JSONException) {
+              Log.e(name, ex.message, ex)
+            }
+            onError!!.invoke(map)
+          }
+        }, options!!.toHashMap()
+      )
+    } catch(lyraMobException: LyraMobException){
+      var map: WritableMap = WritableNativeMap()
+      try {
+        val error: WritableMap = WritableNativeMap()
+        error.putString("detailErrorCode", lyraMobException.detailErrorCode)
+        error.putString("detailErrorMessage", lyraMobException.detailErrorMessage)
+        error.putString("errorCode", lyraMobException.errorCode)
+        error.putString("errorMessage", lyraMobException.errorMessage)
+        lyraMobException.technicalError?.let { error.putBoolean("technicalError", it) }
+        map.putMap("error", error)
+      } catch (ex: JSONException) {
+        Log.e(name, ex.message, ex)
+      }
+      onError!!.invoke(map)
+    }
   }
   companion object {
     const val NAME = "SdkPaymentModule"
--- example/src/App.tsx
+++ example/src/App.tsx
@@ -2,6 +2,7 @@ import { Text, View, StyleSheet, Pressable, Alert } from 'react-native';
 import {
   initialize,
   getFormTokenVersion,
+  process,
 } from '@lyracom/react-native-sdk-payment-module';
 import Config from './Config';
 import { useCallback } from 'react';
@@ -37,6 +38,21 @@ export default function App() {
       });
   };
 
+  /**
+   * Uses this function to validate the payment using your server: check the response integrity by verifying the hash on your server.
+   * @param {*} paymentResult  The result of SDK process method
+   */
+  const verifyPayment = async (paymentResult: any) => {
+    return fetch(Config.merchantServerUrl + '/verifyResult/INTE01_FRANCE', {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: paymentResult,
+    });
+  };
+
   const handlePay = useCallback(async () => {
     console.log('::::> Pay');
     // 1.Initialize Payment SDK
@@ -57,6 +73,27 @@ export default function App() {
     let formToken = await getProcessPaymentContext();
 
     console.log('Form token :', formToken);
+
+    // 3. Call the PaymentSDK process method
+    process(
+      formToken!,
+      {},
+      async (result) => {
+        // onSuccess
+        //4. Verify the payment using your server
+        verifyPayment(result.response)
+          .then(() => {
+            Alert.alert('Payment success');
+          })
+          .catch(() => {
+            Alert.alert('Payment verification fail');
+          });
+      },
+      async (result) => {
+        // onError
+        Alert.alert(result.error.errorMessage);
+      }
+    );
   }, []);
 
   return (
