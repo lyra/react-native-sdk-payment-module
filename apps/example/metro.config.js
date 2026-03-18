b1eab3d30a83dbe8c526cce6bd54867ac9ef3df7
Thibaut-Mejane <thibaut.mejane@gmail.com>
2026-03-18 15:29:34 +0100

    chore: move example to apps folder

--- apps/example/metro.config.js
+++ apps/example/metro.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 const { getDefaultConfig } = require('@react-native/metro-config');
 const { withMetroConfig } = require('react-native-monorepo-config');
 
-const root = path.resolve(__dirname, '..');
+const root = path.resolve(__dirname, '../..');
 
 /**
  * Metro configuration
